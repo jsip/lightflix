48b738bb0b0352af485814d43b58a8c357b69f52
BuildTools <jsimonroyer@gmail.com>
2021-08-16 00:23:17 -0400

    i18n removal

--- next.config.js
+++ next.config.js
@@ -2,9 +2,5 @@ module.exports = {
   reactStrictMode: false,
   env: {
     TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
-  },
-  i18n: {
-    defaultLocale: "fr",
-    locales: ["fr"],
-  },
+  }
 };
